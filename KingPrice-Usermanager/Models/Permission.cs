a28171a1cc3992de1c782ffb06b17b718b9d4278
Gerhard van Zyl <gerhard@schoongezicht.com>
2024-08-25 16:42:28 +0200

    Group.cs: Changed "Type" to "Name" Added Default values to Lists
    Permission.cs:
    Changed "Type" to "Name"
    Added Default value to List
    
    User.cs
    Added Default value to List
    Added ToString Override

--- KingPrice-Usermanager/Models/Permission.cs
+++ KingPrice-Usermanager/Models/Permission.cs
@@ -3,6 +3,6 @@ namespace MrPrice_Usermanager.Models;
 public class Permission
 {
     public int Id { get; set; }
-    public string Type { get; set; }
-    public List<Group> Groups { get; set; }
+    public string Name { get; set; }
+    public virtual List<Group> Groups { get; set; } = new();
 }
